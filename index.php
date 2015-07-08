71eaa814e4b3e725c71f16738706dcb137501ec0
nkulavic <owner2@Nicks-MacBook-Pro.local>
2015-07-08 15:36:20 -0500

    Better docs

--- index.php
+++ index.php
@@ -3,9 +3,17 @@ require_once("src/isdk.php");
 $app = new iSDK;
 $app_name = ''; // Replace with Your Infusionsoft APP Name
 $app_key = ''; // Replace with Your Infusionsoft API Key
+if(empty($app_name)) {
+	echo 'Your APP Name is empty. Please set your app name to your Infusionsoft API name.<br>';
+	return;
+}
+if(empty($app_key)) {
+	echo 'Your APP key is empty. Please set your app key to your Infusionsoft API key.<br>';
+	return;
+}
 if ($app->cfgCon($app_name, $app_key)) {
 	// Connected to Infusionsoft - Run Your Code Below
 } else {
-	echo 'If you are seeing this we are could not connect to Infusionsoft. You must enter in your Infusionsoft APP name and API Key into this file.';	
+	echo 'Error connecting to Infusionsoft. Please make sure your keys are correct.';	
 }
 ?>
