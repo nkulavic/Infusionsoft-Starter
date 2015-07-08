e38d33c974393999a7a8ccd7acb64fe41cb41e39
nkulavic <owner2@Nicks-MacBook-Pro.local>
2015-07-08 15:31:02 -0500

    Setup Info
    Added setup info

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-
 <?
-
 require_once("src/isdk.php");
 $app = new iSDK;
 $app_name = ''; // Replace with Your Infusionsoft APP Name
 $app_key = ''; // Replace with Your Infusionsoft API Key
 if ($app->cfgCon($app_name, $app_key)) {
 	// Connected to Infusionsoft - Run Your Code Below
+} else {
+	echo 'If you are seeing this we are could not connect to Infusionsoft. You must enter in your Infusionsoft APP name and API Key into this file.';	
 }
 ?>
